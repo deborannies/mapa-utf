ed784b50344d6132b0c5957e4d5dd9a48548831e
deborannies <deboracristinaannies@alunos.utfpr.edu.br>
2026-05-07 11:28:29 -0300

    feat: cria componentes personalizados CardAula e PinoMapa

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,31 @@
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import CardAula from '../../components/CardAula';
 
-export default function MinhaGrade() {
+export default function TelaTeste() {
   return (
-    <View>
-      <Text>Minha Grade</Text>
+    <View style={styles.container}>
+      <CardAula 
+        titulo="Pensamento Computacional" 
+        horario="18:40 - 20:10" 
+        local="Bloco B - Lab B6" 
+        ativo={true} 
+      />
+
+      <CardAula 
+        titulo="Introdução à Computação" 
+        horario="20:30 - 22:00" 
+        local="Bloco C - Lab C1" 
+        ativo={false} 
+      />
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f8f9fb',
+    justifyContent: 'center',
+  }
+});
--- app/(tabs)/mapa.tsx
+++ app/(tabs)/mapa.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from "react-native";
+import PinoMapa from "../../components/PinoMapa";
 
 export default function Mapa() {
   return (
-    <View>
-      <Text>Mapa do Campus</Text>
+    <View style={styles.mapaFundo}>
+      
+      <PinoMapa titulo="Bloco A" icone="building" corBase="#8B6B22" top="15%" left="35%" />
+      
+      <PinoMapa titulo="Bloco C" icone="building" corBase="#8B6B22" top="25%" left="15%" />
+      
+      <PinoMapa titulo="Biblioteca" icone="book" corBase="#004b87" top="45%" left="50%" />
+      
+      <PinoMapa titulo="RU" icone="utensils" corBase="#004b87" top="70%" left="70%" />
+
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  mapaFundo: {
+    flex: 1,
+    backgroundColor: "#d1e0d5",
+  },
+});
