46d41c6ee2962075c13eb1608302ddb045d303c3
deborannies <deboracristinaannies@alunos.utfpr.edu.br>
2026-05-08 09:26:24 -0300

    feat: estilizando pagina inicial

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,12 +3,19 @@ import { FontAwesome } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: '#004b87' }}>
+    <Tabs screenOptions={{ 
+        tabBarActiveTintColor: '#004b87', 
+        headerTitleStyle: {
+            color: '#1E3A8A',
+            fontSize: 20,
+        }, 
+      }}>
 
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Minha Grade',
+          headerTitle: 'MapaUTF',
+          tabBarLabel: 'Minha Grade',
           tabBarIcon: ({ color }) => <FontAwesome name="calendar" size={18} color={color} />,
         }}
       />
@@ -16,7 +23,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="mapa"
         options={{
-          title: 'Mapa do Campus',
+          headerTitle: 'MapaUTF',
+          tabBarLabel: 'Mapa do Campus',
           tabBarIcon: ({ color }) => <FontAwesome name="map" size={18} color={color} />,
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,30 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import CardAula from '../../components/CardAula';
 import BotaoFlutuante from '../../components/BotaoFlutuante';
 
 export default function MinhaGrade() {
   return (
     <View style={styles.container}>
+
+      <Text style={styles.labelSaudacao}>Bem-Vindo</Text>
+      <Text style={styles.tituloPrincipal}>Olá, Calouro</Text>
+      <Text style={styles.textoData}>Segunda-Feira, 15 de Abril</Text>
+
+      <Text style={styles.tituloSecao}>Aulas de Hoje</Text>
+
       <CardAula 
         titulo="Pensamento Computacional" 
         horario="18:40 - 20:10" 
         local="Bloco B - Lab B6" 
-        ativo={true} 
-      />
+        ativo={true}/>
 
       <CardAula 
         titulo="Introdução à Computação" 
         horario="20:30 - 22:00" 
         local="Bloco C - Lab C1" 
-        ativo={false} 
-      />
+        ativo={false}/>
 
       <BotaoFlutuante href="/nova-aula" />
-
     </View>
 
     
@@ -30,8 +34,35 @@ export default function MinhaGrade() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 24,
     backgroundColor: '#f8f9fb',
-    justifyContent: 'center',
+  },
+
+  labelSaudacao: {
+    color: '#8B6B22',
+    textTransform: 'uppercase',
+    fontSize: 10,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+
+  tituloPrincipal: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#111827',
+    marginBottom: 4,
+  },
+
+  textoData: {
+    fontSize: 14,
+    color: '#6b7280',
+    marginBottom: 20,
+  },
+
+  tituloSecao: {
+    fontSize: 18,
+    color: '#111827',
+    fontWeight: 'bold',
+    marginBottom: 14,
   }
 });
